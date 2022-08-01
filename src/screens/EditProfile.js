77d0c536cde95a34ff292e8ba87621d90186ffdf
Wegentum Tech <nitesh@wegentumtech.com>
2022-08-01 19:10:18 +0530

    updated for today

--- src/screens/EditProfile.js
+++ src/screens/EditProfile.js
@@ -1,43 +1,74 @@
 import {View, Text, Image, TouchableOpacity, TextInput} from 'react-native';
-import React from 'react';
+import React, {useRef, useState, useEffect} from 'react';
 import AntDesign from 'react-native-vector-icons/AntDesign';
+import Entypo from 'react-native-vector-icons/Entypo';
 import PressBack from '../components/Reusable/PressBack';
 import styles from '../styles/globalStyles';
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
+import RBSheet from 'react-native-raw-bottom-sheet';
 
 const EditProfile = () => {
+  const [imageLink, setImageLink] = useState('');
+  const refRBSheet = useRef();
+
+  const handleCameraOpen = async () => {
+    const result = await launchCamera();
+    console.log(result);
+    setImageLink(result.assets[0].uri);
+  };
+
+  const handleGalleryOpen = async () => {
+    const result = await launchImageLibrary();
+    console.log(result);
+    setImageLink(result.assets[0].uri);
+  };
+
   return (
     <View style={{backgroundColor: 'white', height: '100%'}}>
       <PressBack />
 
       <View style={{marginTop: 50}}>
-        <Image
-          source={{
-            uri: 'https://instagram.fdel8-1.fna.fbcdn.net/v/t51.2885-19/280192147_514591870408592_8565636030206285504_n.jpg?stp=dst-jpg_s150x150&_nc_ht=instagram.fdel8-1.fna.fbcdn.net&_nc_cat=108&_nc_ohc=Pvd4m2LW-RUAX_C5Cy1&edm=APU89FABAAAA&ccb=7-5&oh=00_AT_845unjkP3p96evdu6Ame3i3EhRxoBjcfciRXzaN_hyg&oe=62EF3609&_nc_sid=86f79a',
-          }}
-          style={{
-            width: 120,
-            height: 120,
-            borderRadius: 100,
-            top: -45,
-            alignSelf: 'center',
-          }}
-        />
-<TouchableOpacity>
-        <View
-          style={{
-            alignSelf: 'center',
-            top: -90,
-            left: 50,
-            backgroundColor: '#F79320',
-            padding: 5,
-            borderRadius: 30,
-          }}>
-          <AntDesign name="camera" size={18} color="white" />
-        </View>
-
-</TouchableOpacity>
+        {imageLink ? (
+          <Image
+            source={{
+              uri: imageLink,
+            }}
+            style={{
+              width: 120,
+              height: 120,
+              borderRadius: 100,
+              top: -45,
+              alignSelf: 'center',
+            }}
+          />
+        ) : (
+          <Image
+            source={{
+              uri: 'https://instagram.fdel8-1.fna.fbcdn.net/v/t51.2885-19/280192147_514591870408592_8565636030206285504_n.jpg?stp=dst-jpg_s150x150&_nc_ht=instagram.fdel8-1.fna.fbcdn.net&_nc_cat=108&_nc_ohc=Pvd4m2LW-RUAX_C5Cy1&edm=APU89FABAAAA&ccb=7-5&oh=00_AT_845unjkP3p96evdu6Ame3i3EhRxoBjcfciRXzaN_hyg&oe=62EF3609&_nc_sid=86f79a',
+            }}
+            style={{
+              width: 120,
+              height: 120,
+              borderRadius: 100,
+              top: -45,
+              alignSelf: 'center',
+            }}
+          />
+        )}
+        <TouchableOpacity onPress={() => refRBSheet.current.open()}>
+          <View
+            style={{
+              alignSelf: 'center',
+              top: -90,
+              left: 50,
+              backgroundColor: '#F79320',
+              padding: 5,
+              borderRadius: 30,
+            }}>
+            <AntDesign name="camera" size={18} color="white" />
+          </View>
+        </TouchableOpacity>
       </View>
-
 
       <View style={{marginHorizontal: 30, marginTop: 20}}>
         <View>
@@ -101,6 +132,50 @@ const EditProfile = () => {
         <TouchableOpacity>
           <Text style={styles.button2}>Save</Text>
         </TouchableOpacity>
+
+        <RBSheet
+          animationType="slide"
+          height={130}
+          ref={refRBSheet}
+          closeOnDragDown={true}
+          closeOnPressMask={true}
+          customStyles={{
+            wrapper: {
+              backgroundColor: 'transparent',
+            },
+            draggableIcon: {
+              backgroundColor: '#86868C',
+            },
+          }}>
+          <View>
+            <View
+              style={{
+                marginHorizontal: 10,
+                flexDirection: 'row',
+                marginTop: 20,
+              }}>
+              <TouchableOpacity onPress={handleCameraOpen} style={{flex: 1}}>
+                <View style={{flexDirection: 'row', justifyContent: 'center'}}>
+                  <AntDesign name="camera" size={30} color="black" />
+                  <Text
+                    style={{fontSize: 18, color: 'black', fontWeight: 'bold'}}>
+                    {'   '}Camera
+                  </Text>
+                </View>
+              </TouchableOpacity>
+
+              <TouchableOpacity onPress={handleGalleryOpen} style={{flex: 1}}>
+                <View style={{flexDirection: 'row', justifyContent: 'center'}}>
+                  <Entypo name="folder-images" size={30} color="black" />
+                  <Text
+                    style={{fontSize: 18, color: 'black', fontWeight: 'bold'}}>
+                    {'   '}Gallery
+                  </Text>
+                </View>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </RBSheet>
       </View>
     </View>
   );
--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,25 +1,22 @@
 import {View, Text, TextInput} from 'react-native';
-import React,{useState,useEffect,useRef} from 'react';
+import React, {useState, useEffect, useRef} from 'react';
 import styles from '../styles/globalStyles';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import AntDesign from 'react-native-vector-icons/AntDesign';
 import Tags from '../components/Home/Tags';
-import { useNavigation } from '@react-navigation/native';
-
+import {useNavigation} from '@react-navigation/native';
 
 const SearchScreen = () => {
-    const navigation = useNavigation();
-  
-    
+  const navigation = useNavigation();
+
   const pin1Ref = useRef(null);
 
-
   return (
-    <View style={{paddingTop: 20, backgroundColor: '#E38922', height: '100%'}}>
+    <View style={{paddingTop: 20, backgroundColor: '#F26227', height: '100%'}}>
       <View style={{flexDirection: 'row', marginHorizontal: 10}}>
         <Text style={{flex: 1}}>
           <AntDesign
-          onPress={()=>navigation.goBack()}
+            onPress={() => navigation.goBack()}
             name="leftcircle"
             style={{borderRadius: 10}}
             size={35}
@@ -31,7 +28,7 @@ const SearchScreen = () => {
 
       <View style={{alignItems: 'center', marginTop: 10}}>
         <TextInput
-        autoFocus
+          autoFocus
           style={styles.inputBox}
           placeholder="   Search foods which you like"
           placeholderTextColor="#4F4F4F"
